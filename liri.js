607c8fe3563327404c6583a96790a40fa3c5a301
Natalia Malcova <nataliamalkova@yahoo.com>
2019-07-29 13:17:53 -0400

    edited files

--- liri.js
+++ liri.js
@@ -1,10 +1,8 @@
 
-
+var fs = require("fs");
 var dotenv = require("dotenv").config();
 
-var keys = require("./keys.js");
-var Spotify = require("node-spotify-api");
-var spotify = new Spotify(keys.spotify);
+
 
 
 var action = process.argv[2];
@@ -30,20 +28,25 @@ switch(action) {
 
 
 function spotifySong() {
+
+  var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+
   spotify
-  .request('https://api.spotify.com' + value)
+  .request('https://api.spotify.com/v1/tracks' + value)
   .then(function(data) {
-    console.log("Artist(s):" + data.name);
-    console.log("The song's name:" + data.song);
-    console.log("A preview link of the song from:" + data.url);
-    console.log("Album:" + data.album);
+    console.log("Artist(s):" + data.artists);
+    console.log("The song's name:" + data.value);
+    console.log("A preview link of the song from:" + data.uri);
+    console.log("Album:" + data.name);
     // console.log(data); 
   })
   .catch(function(err) {
     console.error('Error occurred: ' + err); 
   });
 }
-console.log(action, value);
+// console.log(action, value);
 
 
 
@@ -54,12 +57,17 @@ console.log(action, value);
 function movieThis() {
   var axios = require("axios");
   axios
-  .get("https://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=9a68ce23")
+  .get("https://www.omdbapi.com/?t=" + value + "&y=&plot=short&tomatoes=true&apikey=9a68ce23")
   .then(function(response) {
     // If the axios was successful then log the body from the site
     console.log("Title of the movie:" + response.data.Title);
     console.log("Release Year:" + response.data.Year);
     console.log("The movie's rating:" + response.data.imdbRating);
+    console.log("Rotten Tomatoes Rating of the movie:" + response.data.tomatoRotten);
+    console.log("Country:" + response.data.Country);
+    console.log("Language:" + response.data.Language);
+    console.log("Plot:" + response.data.Plot);
+    console.log("Actors:" + response.data.Actors);
 
     // console.log(response.data);
   })
@@ -82,5 +90,21 @@ function movieThis() {
 }
 
 
+function whatSays() {
+  fs.readFile("random.txt", "utf8", function(error,data) {
+    if(error) {
+      return console.log(error);
+    }
+    console.log(data);
+  })
 
+  fs.appendFile("random.txt", ", " + value, function(err) {
+    if(err) {
+      console.log(err);
+    }
+    else {
+      console.log("Content Added!");
+    }
+  })
+}
 
